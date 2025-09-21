41b4f2e0f4424cde7378afb755cdeca8d9a7efa3
Muhammad Owais <muhammadowaiskhan616@gmail.com>
2025-09-21 18:56:13 +0500

    checking

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
-// Coded the whole project in React, used Vite for the build process and used github pages for deployment .
+// Coded the whole project in  React, used Vite for the build process and used github pages for deployment .
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
